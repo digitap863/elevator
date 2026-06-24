3d6ff2d48dc73b70f1b140fa2f4076388618c9ca
Aginas pk <aginaspk6@gmail.com>
2026-06-24 10:52:43 +0530

    GTM: Google Ad Manager

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Lora, Roboto, Work_Sans } from 'next/font/google';
 import localFont from 'next/font/local';
 import "./globals.css";
+import Script from 'next/script';
 
 
 export const metadata = {
@@ -71,10 +72,39 @@ const satoshi = localFont({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5BFCXLJF');
+          `}
+        </Script>
+      </head>
       <body className={`${roboto.variable} ${lora.variable} ${worksans.variable} ${aquire.variable} ${satoshi.variable} ${dragon.variable} antialiased`} suppressHydrationWarning>
-          <div className="overflow-hidden">       
-            {children}
-          </div>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5BFCXLJF"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
+        <div className="overflow-hidden">
+          {children}
+        </div>
       </body>
     </html>
   );
