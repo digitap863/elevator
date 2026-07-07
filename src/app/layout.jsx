0130c445e9c9f313319302f6147c1979fce1b062
muhammed  arshaq tk <arshaqtk4@gmail.com>
2026-07-07 12:59:38 +0530

    feat: initialize root layout with custom font configuration and analytics integration

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -74,6 +74,19 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <head>
         <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KKW0G1DMSH"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-KKW0G1DMSH');
+          `}
+        </Script>
+        <Script
           id="google-tag-manager"
           strategy="afterInteractive"
         >
